4d2da27fd127f2d6512bc23404a4f152cb741327
Nicholas Wong <nicowong8@gmail.com>
2020-10-17 22:32:52 -0700

    Added deleteKey and its helpers

--- TST.cpp
+++ TST.cpp
@@ -195,6 +195,13 @@ Node* TST::insert(std::string key)
     }
 }
 
+// Prints 'key deleted, new count = x' if count > 1; else, 'key deleted'; after deleting
+void TST::deletePrint(std::string key)
+{
+    // Delete key from root then set the new root
+    this->root = deleteKey(key, this->root, true);
+}
+
 // Prints all keys from m to n, double inclusive, in-order
 void TST::rangeSearch(std::string low, std::string high)
 {
@@ -238,6 +245,268 @@ void TST::destroyTree(Node* n)
         delete n;
     }
     
+}
+
+// Deletes key from tree with root n and returns the new root
+// If the isFirstDelete flag is false, then the whole NodeKey is reset/deleted no matter its count
+Node* TST::deleteKey(std::string key, Node* n, bool isFirstDelete)
+{
+    // If n is null or key is empty, return
+    if (n == nullptr || key == "")
+    {
+        return nullptr;
+    }
+
+    // Else if right is empty, then this node has no children
+    else if (n->right.key == "" && n->right.count == 0)
+    {
+        // If left is key, then delete it and possibly delete this node
+        if (n->left.key == key)
+        {
+            // If count > 1 and isFirstDelete is true, then decrement the count
+            if (n->left.count > 1 && isFirstDelete == true)
+            {
+                n->left.count--;
+                printKeyDeleted(n->left, false);    // Print that this key was partially deleted
+                return n;
+            }
+
+            // Else, delete this node and return nullptr as new root
+            else
+            {
+                // If isFirstDelete is true, then print that this key was deleted
+                if (isFirstDelete == true)
+                {
+                    printKeyDeleted(n->left, true); // Print that this key was fully deleted
+                }
+                
+                delete n;
+                return nullptr;
+            }
+        }
+
+        // Else, key is not in the tree so stop and return this node as new root
+        else
+        {
+            return n;
+        }
+    }
+
+    // Else if left is key
+    else if (n->left.key == key)
+    {
+        bool fullyDeleted = true;   // Tracks whether key was fully deleted for correct printing
+        NodeKey deletedKey = n->left; // Tracks the key that is being deleted
+
+        // If count > 1 and isFirstDelete is true, then decrement the count
+        if (n->left.count > 1 && isFirstDelete == true)
+        {
+            n->left.count--;
+            deletedKey = n->left;   // Update the deleted key
+            fullyDeleted = false;   // Mark that it was not fully deleted
+        }
+
+        // Else if left subtree exists,
+        else if (n->leftChild != nullptr)
+        {
+            // Set left to max of left subtree
+            n->left = findMax(n->leftChild);
+
+            // Delete the max from the left subtree and set the left child to the new root
+            n->leftChild = deleteKey(n->left.key, n->leftChild, false);
+        }
+
+        // Else if middle subtree exists,
+        else if (n->midChild != nullptr)
+        {
+            // Set left to min of middle subtree
+            n->left = findMin(n->midChild);
+
+            // Delete the min from the middle subtree and set the left child to the new root
+            n->midChild = deleteKey(n->left.key, n->midChild, false);
+        }
+
+        // Else, fill in from right to left
+        else
+        {
+            // Set the left to the right
+            n->left = n->right;
+
+            // Set the right to the min of the right subtree
+            n->right = findMin(n->rightChild);
+
+            // Delete the min from the right child and set the right child to the new root
+            n->rightChild = deleteKey(n->right.key, n->rightChild, false);
+        }
+
+        // If this is the first delete, then print that this key was deleted
+        if (isFirstDelete == true)
+        {
+            printKeyDeleted(deletedKey, fullyDeleted);
+        }
+
+        // Return this node as the root
+        return n;
+    }
+
+    // Else if right is key
+    else if (n->right.key == key)
+    {
+        bool fullyDeleted = true;   // Tracks whether key was fully deleted for correct printing
+        NodeKey deletedKey = n->right;  // Tracks the key that is being deleted
+
+        // If count > 1 and isFirstDelete is true, then decrement the count
+        if (n->right.count > 1 && isFirstDelete == true)
+        {
+            n->right.count--;
+            deletedKey = n->right;  // Updates deleted key
+            fullyDeleted = false;   // Sets fullyDeleted to false
+        }
+
+        // Else if middle subtree exists,
+        else if (n->midChild != nullptr)
+        {
+            // Set right to the max of the middle subtree
+            n->right = findMax(n->midChild);
+
+            // Delete the max from the middle subtree and set the middle child to the new root
+            n->midChild = deleteKey(n->right.key, n->midChild, false);
+        }
+
+        // Else if right subtree exists,
+        else if (n->rightChild != nullptr)
+        {
+            // Set right to the min the of right subtree
+            n->right = findMin(n->rightChild);
+
+            // Delete the min from the right subtree and set the right child to the new root
+            n->rightChild = deleteKey(n->right.key, n->rightChild, false);
+        }
+
+        // Else if left subtree exists, fill in from left to right
+        else if (n->leftChild != nullptr)
+        {
+            // Set the right to the left
+            n->right = n->left;
+
+            // Set the left to the max of the left subtree
+            n->left = findMax(n->leftChild);
+
+            // Delete the max from the left subtree and set the left child to the new root
+            n->leftChild = deleteKey(n->left.key, n->leftChild, false);
+        }
+
+        // Else, reset/delete the right key
+        else
+        {
+            n->right = NodeKey("", 0);
+        }
+
+        // If this is the first delete, then print that this key was deleted
+        if (isFirstDelete == true)
+        {
+            printKeyDeleted(deletedKey, fullyDeleted);
+        }
+
+        // Return this node as the root
+        return n;
+    }
+
+    // Else if key < left key, then delete the key from left subtree
+    else if (key < n->left.key)
+    {
+        // Delete the key from left subtree and set the left child to the new root
+        n->leftChild = deleteKey(key, n->leftChild, isFirstDelete);
+        return n;
+    }
+
+    // Else if key < right key, then delete the key from middle subtree
+    else if (key < n->right.key)
+    {
+        // Delete the key from middle subtree and set the middle child to the new root
+        n->midChild = deleteKey(key, n->midChild, isFirstDelete);
+        return n;
+    }
+
+    // Else, the key can only be in right subtree
+    else
+    {
+        // Delete the key from the right subtree and set the right child to the new root
+        n->rightChild = deleteKey(key, n->rightChild, isFirstDelete);
+        return n;
+    }
+}
+
+// If the fullyDeleted flag is true, then prints 'key deleted'; else, prints 'key deleted, new count = x'
+void TST::printKeyDeleted(NodeKey const& nk, bool fullyDeleted)
+{
+    if (fullyDeleted == true)
+    {
+        std::cout << nk.key << " deleted" << std::endl;
+    }
+
+    else
+    {
+        std::cout << nk.key << " deleted, new count = " << nk.count << std::endl;
+    }
+}
+
+// Finds the minimum key from tree with root n
+NodeKey TST::findMin(Node* n)
+{
+    // If n is null, then return an empty key
+    if (n == nullptr)
+    {
+        return NodeKey("", 0);
+    }
+    
+    // Else, Traverse left until it is no longer possible
+    while (true)
+    {
+        // If the left subtree exists, then move to the left child
+        if (n->leftChild != nullptr)
+        {
+            n = n->leftChild;
+        }
+
+        // Else, return the left key
+        else
+        {
+            return n->left;
+        }
+    }
+}
+
+// Finds the maximum key from tree with root n
+NodeKey TST::findMax(Node* n)
+{
+    // If n is null, then return an empty key
+    if (n == nullptr)
+    {
+        return NodeKey("", 0);
+    }
+
+    // Else, traverse right until it is no longer possible
+    while (true)
+    {
+        // If the right subtree exists, then move to the right child
+        if (n->rightChild != nullptr)
+        {
+            n = n->rightChild;
+        }
+
+        // Else if right is not empty, then return right
+        else if (n->right.key != "" && n->right.count > 0)
+        {
+            return n->right;
+        }
+
+        // Else, return left
+        else
+        {
+            return n->left;
+        }
+    }
 }
 
 // Prints all keys from lo to hi, double inclusive, in-order with root node n
