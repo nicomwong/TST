9bffe5020a710fbee05bbf9891fc3cbbbd06b311
Nicholas Wong <nicowong8@gmail.com>
2020-10-18 13:43:24 -0700

    made main() return 0 instead; was causing error before

--- TSTTest.cpp
+++ TSTTest.cpp
@@ -167,7 +167,7 @@ int main(int argc, char* argv[])
     if (argc != 2)
     {
         std::cout << "Usage: ./TSTTest testName" << std::endl;
-        return 0;
+        return -1;
     }
 
     std::string testName = argv[1];
@@ -196,5 +196,5 @@ int main(int argc, char* argv[])
         testDestructor();
     }
 
-    return 1;
+    return 0;
 }
